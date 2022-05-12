cb55a41cf5b06297bb91706b34cbf9d89db81084
Jean-RodBR <101993941+Jean-RodBR@users.noreply.github.com>
2022-05-12 19:14:11 -0300

    Update sec_ext_ric.r

--- sec_ext_ric.r
+++ sec_ext_ric.r
@@ -1,7 +1,7 @@
 #usando código do bruno para extrair apenas seções
 
-
 library(magrittr)
+
 base2<-db_statutes$statutes_complete %>% as.character() %>% tm::stripWhitespace()
 titulo_names <- list()
 capitulo_names <- list()
@@ -51,6 +51,7 @@ rm(session.save.capitulo,session.save.titulo)
 #todo nome acaba antes de um art° de acordo com exemplo
 #Vou fazer o programa pegar do começo da string até o simbolo 
 save.sec_noms <- list()
+save.sec_noms2 <- list()
 save.art_sep <- list()
 noms_sec <- c()
 cap_get <- c()
@@ -64,35 +65,43 @@ for(j in 1:m){
     for(i in 1:n){
       art_sep[[i]] <-  cap_get[i,1] %>% stringr::str_split(.,"Art @a.") #separa por artigos
       noms_sec[i]   <-  art_sep[[i]][[1]][1]
-      {#TIRANDO ESSES NUMEROS ROMANOS
-        x <- noms_sec[i] %>% stringr::str_extract_all(.,"\\w+") %>% unlist()
-        x <- x[-1]
-        x <- paste(x,collapse=" ")
-        noms_sec[i] <- x
-      }
+      # {#TIRANDO ESSES NUMEROS ROMANOS
+      #   x <- noms_sec[i] %>% stringr::str_extract_all(.,"\\w+") %>% unlist()
+      #   x <- x[-1]
+      #   x <- paste(x,collapse=" ")
+      #   noms_sec[i] <- x
+      # }
     }
-    save.sec_noms[[j]] <- noms_sec #separado por sessão
     save.art_sep[[j]] <- art_sep #separação por artigos
+    save.sec_noms[[j]] <- noms_sec %>% data.frame() #separado por sessão
+    
+    save.sec_noms2[[j]] <- cbind(db_cap_titulos[[2]][[j]],save.sec_noms[[j]])
     noms_sec <- c()
     art_sep <- c()
+    
   }
 }
+
+
 #pegando elementos não nulos do resultado
-save.sec_noms <- save.sec_noms %>% .[. != "NULL"]
+save.sec_noms2 <- save.sec_noms2 %>% .[. != "NULL"]
 
-noms_change <- save.sec_noms %>%  unlist() %>% unique() %>% data.frame() #colocando tudo em um data.frame depois dando unique
+
+  x <- do.call(rbind,save.sec_noms2)
+  
+  
+  
+noms_change <- x[,2] %>%  unlist() %>% unique() %>% data.frame() #colocando tudo em um data.frame depois dando unique
 #Agora pegar esse resultado, comparar manualmente (n sei outro jeito) e criar um sistema de classificação
+#noms_change <-noms_change %>% stringr::str_detect(.,"ESTATUTO")
 
-rio::export(noms_change,"sec_noms_change.xlsx")
 
 #_______________________________________________________________________________
-sec_noms_change2 <- readxl::read_excel("sec_noms_change2.xlsx")
-sec_pure <- sec_noms_change2$Alterado %>% toupper() %>% unique()  %>% .[order(nchar(.), .)]
+rio::export(noms_change,"sec_noms_change.xlsx")
+sec_noms_change2 <- readxl::read_excel("sec_noms_change2.xlsx") #manualmente criar uma coluna do lado com o padrão desejado
+sec_pure <- sec_noms_change2$ALTERADO %>% unique()  %>% .[order(nchar(.), .)]
 rio::export(sec_pure,"add-cod.xlsx")
 #_______________________________________________________________________________
-
-
-
 
 
 
